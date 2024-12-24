a28c048606f9768a20ccc3b280285c4ad2cb1761
TranTheKy <ttk8701ad@gmail.com>
2024-12-24 15:22:21 +0700

    ExamMudule4

---
+++ src/test/java/com/exam/ExamModule4/ExamModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.ExamModule4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamModule4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
